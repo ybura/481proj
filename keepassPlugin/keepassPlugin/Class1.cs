c9a33f420d619dd796564827f919fa6215f66d83
ybura <ybura@umich.edu>
2016-10-12 09:18:09 -0400

    Added a bunch of shit
    Added our plugin project and built source so that the exe reference in
    the project should be the same one I used. Plugin doesn't do anything,
    compiles but I can't figure out how to read parts of entries, just how
    to add them but that's useless to us rn. Also added the sample plugin
    source but that doesn't have anything on reading entries either but
    could be useful.

---
+++ keepassPlugin/keepassPlugin/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System.Speech.Synthesis;
+using KeePass.Plugins;
+using KeePass.Forms;
+using KeePassLib;
+
+namespace keepassPlugin
+{
+    public sealed class SamplePluginExt : Plugin
+    {
+        private IPluginHost m_host = null;
+
+        public override bool Initialize(IPluginHost host)
+        {
+            m_host = host;
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+            synthesizer.Volume = 100;  // 0...100
+            synthesizer.Rate = -2;     // -10...10
+
+            // Synchronous
+            PwEntry current_entry;
+            synthesizer.Speak("Foo");
+
+            return true;
+        }
+    }
+}
