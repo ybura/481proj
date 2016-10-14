6882b547398243dda7d75bdd3c0c8e92193dd46f
ybura <ybura@umich.edu>
2016-10-14 12:36:05 -0400

    Removed keepass src, updated our plugin file
    Plugin file builds on Windows 10 on my laptop, hopefully builds on other
    systems.

---
+++ keepassPlugin/keepassPlugin/keepassPlugin.cs
@@ -0,0 +1,28 @@
+﻿using System.Speech.Synthesis;
+using KeePass.Plugins;
+using KeePassLib;
+
+namespace keepassPlugin
+{
+    public sealed class SamplePluginExt : Plugin
+    {
+        private IPluginHost m_host = null;
+
+        public override bool Initialize(IPluginHost host)
+        {
+            m_host = host;
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+            synthesizer.Volume = 100;  // 0...100
+            synthesizer.Rate = -2;     // -10...10
+
+            // Synchronous
+            PwEntry current_entry;
+            synthesizer.Speak("Hello");
+
+            return true;
+        }
+        public override void Terminate()
+        {   
+        }
+    }
+}
