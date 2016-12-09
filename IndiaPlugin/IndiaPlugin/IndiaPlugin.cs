25f40af4111404c03839f6520be624dbe3176fcf
ybura <ybura@umich.edu>
2016-12-09 14:58:59 -0500

    Implemented fixes/suggestions from latest design review and bug reports

--- IndiaPlugin/IndiaPlugin/IndiaPlugin.cs
+++ IndiaPlugin/IndiaPlugin/IndiaPlugin.cs
@@ -4,10 +4,16 @@ using System.Drawing;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 
-using KeePass.Plugins;
-using KeePassLib;
 using KeePass.UI;
 using KeePass;
+
+
+using KeePass.Plugins;
+using KeePass.Forms;
+using KeePass.Resources;
+
+using KeePassLib;
+using KeePassLib.Security;
 
 namespace IndiaPlugin
 {
@@ -15,12 +21,10 @@ namespace IndiaPlugin
     {
         private IPluginHost m_host = null;
 
-        private ToolStripSeparator m_tsSeparator = null;
-        private ToolStripMenuItem m_tsmiPopup = null;        
-
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
         [DllImport("user32.dll")]
         public static extern int SendMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
-
+        
         // On creation of plugin, set-up resources.
         public override bool Initialize(IPluginHost host)
         {
@@ -29,32 +33,23 @@ namespace IndiaPlugin
             m_host = host;
 
             m_host.MainWindow.UIStateUpdated += this.OnUIStateUpdated;
-            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+
             synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
             synthesizer.Volume = 100;  // 0...100
             synthesizer.Rate = 0;     // -10...10
-            synthesizer.Speak("Hi India.");
-
-            ToolStripItemCollection tsMenu = m_host.MainWindow.ToolsMenu.DropDownItems;
+            synthesizer.SpeakAsync("Hi India. Type your password and press enter.");
+            
 
             m_host.MainWindow.KeyPreview = true;
             m_host.MainWindow.KeyDown += SpeakSelectedEntry;
             m_host.MainWindow.KeyDown += SpeakAllEntries;
             m_host.MainWindow.KeyDown += HelpMenu;
-            // Add a separator at the bottom
-            m_tsSeparator = new ToolStripSeparator();
-            tsMenu.Add(m_tsSeparator);
+            m_host.MainWindow.KeyDown += KillSpeech;
 
-            // Add the popup menu item
-            m_tsmiPopup = new ToolStripMenuItem();
-            m_tsmiPopup.Text = "Plugin for India";
-            tsMenu.Add(m_tsmiPopup);
-            
-            // TODO: Set-up hotkey options for plugin.
-            //m_hkGlobalAutoType.HotKey = (kAT & Keys.KeyCode);
-            //m_hkGlobalAutoType.HotKeyModifiers = (kAT & Keys.Modifiers);
-            //m_hkGlobalAutoType.RenderHotKey();
-           
+            m_host.MainWindow.EntryContextMenu.KeyDown += SpeakAddEntryMenu;
+            m_host.MainWindow.EntryContextMenu.KeyDown += KillSpeech;
+
+
             return true;
        } 
 
@@ -67,29 +62,54 @@ namespace IndiaPlugin
 
             m_host = null;
         }
-        
+
+        private void SpeakAddEntryMenu(object sender, KeyEventArgs e)
+        {
+            synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
+            synthesizer.Volume = 100;  // 0...100
+            synthesizer.Rate = 0;     // -10...10
+
+            ToolStripItemCollection tsMenu = m_host.MainWindow.EntryContextMenu.Items;
+
+            if (e.KeyCode == Keys.I && e.Control && e.Shift)
+            {
+                synthesizer.SpeakAsync("yo");
+                for (int i = 0; i < tsMenu.Count; i++)
+                {
+                    synthesizer.SpeakAsync(tsMenu[i].Text);
+                }
+                synthesizer.SpeakAsync(tsMenu.Count.ToString());
+            }
+        }
+
         private void HelpMenu(object sender, KeyEventArgs e)
         {
-            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
             synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
             synthesizer.Volume = 100;  // 0...100
             synthesizer.Rate = 0;     // -10...10
 
             if (e.KeyCode == Keys.H && e.Control)
             {
-                synthesizer.Speak("These are the keyboard options: press up or down to highlight an entry and have it spoken out, press control E to hear all the entries in the database, press control U to open the URL of the selected entry, press control H to hear this help menu again.");
+                synthesizer.SpeakAsync("These are the keyboard options: press up or down to highlight an entry and have it spoken out, press control E to hear all the entries in the database, press control I to add a new entry, press control U to open the URL of the selected entry,press control H to hear this help menu again.");
             }
         }
 
+        private void KillSpeech(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyCode == Keys.K && e.Control && e.Shift) || e.KeyCode == Keys.Enter)
+            {
+                synthesizer.SpeakAsyncCancelAll();
+            }
+        }
+        
         private void SpeakSelectedEntry(object sender, KeyEventArgs e)
         {
-            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
             synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
             synthesizer.Volume = 100;  // 0...100
             synthesizer.Rate = 0;     // -10...10
             if (!m_host.Database.IsOpen)
             {
-                synthesizer.Speak("You first need to open a database!");
+                synthesizer.SpeakAsync("You first need to open a database!");
                 return;
             }
 
@@ -98,35 +118,42 @@ namespace IndiaPlugin
             System.Windows.Forms.ListView.SelectedIndexCollection selected_index = lv.SelectedIndices;
 
             if (e.KeyCode == Keys.Up)
-            {                
+            {
+                synthesizer.SpeakAsyncCancelAll();
                 for (int i = 0; i < lv.Items.Count; i++)
                 {
                     if (selected_index.Contains(i))
                     {
                         if (i - 1 < 0)
                         {
-                            synthesizer.Speak(lv.SelectedItems[0].Text);
+                            synthesizer.SpeakAsync(lv.SelectedItems[0].Text);
                         }
                         else
                         {
-                            synthesizer.Speak(lv.Items[i - 1].Text);
+                            synthesizer.SpeakAsync(lv.Items[i - 1].Text);
                         }
                     }
                 }
             }
             if (e.KeyCode == Keys.Down)
             {
+                synthesizer.SpeakAsyncCancelAll();
+                if (lv.Items.Count == 0)
+                {
+                    lv.Focus();
+                    lv.Items[0].Selected = true;
+                }
                 for (int i = 0; i < lv.Items.Count; i++)
                 {
                     if (selected_index.Contains(i))
                     {
                         if (i + 1 >= lv.Items.Count)
                         {
-                            synthesizer.Speak(lv.SelectedItems[0].Text);
+                            synthesizer.SpeakAsync(lv.SelectedItems[0].Text);
                         }
                         else
                         {
-                            synthesizer.Speak(lv.Items[i + 1].Text);
+                            synthesizer.SpeakAsync(lv.Items[i + 1].Text);
                         }
                     }
                 }
@@ -136,14 +163,14 @@ namespace IndiaPlugin
         // Dictates all the items in the password listview.
         private void SpeakAllEntries(object sender, KeyEventArgs e)
         {
-            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+             
             synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
             synthesizer.Volume = 100;  // 0...100
             synthesizer.Rate = 0;     // -10...10
             
             if (!m_host.Database.IsOpen)
             {
-                synthesizer.Speak("You first need to open a database!");
+                synthesizer.SpeakAsync("You first need to open a database!");
                 return;
             }
 
@@ -152,10 +179,10 @@ namespace IndiaPlugin
 
             if (e.KeyCode == Keys.E && e.Control)
             {
-                synthesizer.Speak("I am about to tell you the current entries.");
+                synthesizer.SpeakAsync("I am about to tell you the current entries.");
                 for (int i = 0; i < lv.Items.Count; i++)
                 {
-                    synthesizer.Speak(lv.Items[i].Text);
+                    synthesizer.SpeakAsync(lv.Items[i].Text);
                 }
             }           
 
@@ -203,6 +230,7 @@ namespace IndiaPlugin
                 else if (i % 2 == 1)
                 {
                     lv.Items[i].BackColor = System.Drawing.Color.Blue;
+                    lv.Items[i].ForeColor = System.Drawing.Color.WhiteSmoke;
                 }
             }
 
